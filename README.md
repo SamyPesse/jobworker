5aee07b27f3f313b32f80a8c4c28ae8bc08489b0
Samy Pessé <samypesse@gmail.com>
2015-05-08 19:16:30 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# jobworker
-Webworker background job queue
+JobWorker
+=============================
+
+> Utility to create a job queue in a webworker
+
+## Installation
+
+```
+$ npm install jobworker
+```
+
+### Documentation
+
+Create a web-worker "myworker.js" using:
+
+```js
+var JobWorker = require("jobworker");
+
+var worker = new JobWorker();
+
+// Register some methods
+worker.register({
+    // Task can be sync
+    hello: function() {
+        return "World";
+    },
+
+    // Or async:
+    testAsync: function() {
+        return doSomethingSync()
+        .then(function() {
+            // it should return a promise
+        });
+    }
+});
+
+// Run the worker
+worker.run();
+```
+
+And in your application, access the web-worker using:
+
+```js
+var worker = new JobWorker({
+    script: "myworker.js"
+});
+
+
+// Call a method
+worker.callMethod("hello")
+.then(function(msg) {
+
+});
+
+// Or create a binded nethod:
+var testAsync = worker.method("testAsync");
+```
+
---
+++ index.js
@@ -0,0 +1,133 @@
+var _ = require("lodash");
+var Q = require("q");
+
+var JobWorker = function() {
+    this.worker = null;
+    this.workerLoading = null;
+    this.methods = {};
+}
+
+
+// Init caller worker
+JobWorker.prototype.getRemoteWorker = function() {
+    var that = this;
+
+    if (this.workerLoading) return this.workerLoading;
+    if (this.worker) return Q(this.workerLoading);
+
+
+    var d = Q.defer();
+    this.workerLoading = d.promise;
+
+    this.worker = new Worker(this.options.script);
+    this.worker.postMessage({
+        message: "init"
+    });
+    this.worker.addEventListener('message', function(e) {
+        if (e.data.message == "ready") {
+            d.resolve(that.worker);
+        } else {
+            that.trigger("task:"+e.data.id, e.data);
+        }
+    }, false);
+
+    return this.workerLoading;
+};
+
+// Call a metod from the worker
+JobWorker.prototype.callMethod = function(method) {
+    var that = this;
+    var args = Array.prototype.slice.call(arguments, 1);
+
+    return that.getRemoteWorker()
+    .then(function(wk) {
+        var d = Q.defer();
+
+        var taskId = _.uniqueId("task");
+        var msg = {
+            message: "call",
+            id: taskId,
+            method: method,
+            arguments: args
+        };
+
+        that.once("task:"+taskId, function(data) {
+            if (data.rejected) {
+                d.reject(new Error(data.rejected));
+            } else {
+                d.resolve(data.resolved);
+            }
+        });
+
+        wk.postMessage(msg)
+
+        return d.promise;
+    });
+};
+
+// Return a method as a function ca be called
+JobWorker.prototype.method = function(method) {
+    return _.partial(_.bind(this.callMethod, this), method);
+};
+
+// Register a method
+JobWorker.prototype.register = function(method, handler) {
+    var that = this;
+
+    if (_.isObject(method)) {
+        _.each(_.methods(method), function(name) {
+            this.register(name, method[name]);
+        }, this);
+        return;
+    }
+
+    this.methods[method] = function() {
+        var args = Array.prototype.slice.apply(arguments);
+
+        return Q()
+        .then(function() {
+            return _.partial.apply(_, [handler].concat(args))();
+        });
+    };
+};
+
+// Run in the worker
+JobWorker.prototype.run = function() {
+    var that = this;
+
+    // Handle message from app
+    addEventListener('message', function(e) {
+        var data = e.data;
+        var method = data.method;
+        var taskId = data.id;
+        var args = data.arguments || [];
+
+        if (!method || !taskId) return;
+
+        Q()
+        .then(function() {
+            if (!that.methods[method]) throw "Method doesn't exist: "+method;
+
+            return that.methods[method].apply(that, args);
+        })
+        .then(function(r) {
+            return {
+                resolved: r
+            };
+        }, function(err) {
+            return Q({
+                rejected: err.toString()
+            });
+        })
+        .then(function(r) {
+            r.id = taskId;
+            r.message = "result";
+            postMessage(r);
+        })
+    }, false);
+
+    // Signal that the worker is ready
+    postMessage({ message: "ready" })
+};
+
+module.exports = JobWorker;
